f8cff1ede39fd4d78c05206834069470bd09b8b2
WaitWalker <waitwalker@163.com>
2017-09-04 18:13:07 +0800

    添加注册登录模块

---
+++ MyTwitter/MyTwitter/Common/Base/MTTTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  MTTTabBarController.swift
+//  MyTwitter
+//
+//  Created by LiuChuanan on 2017/9/4.
+//  Copyright © 2017年 waitWalker. All rights reserved.
+//
+
+import UIKit
+
+class MTTTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

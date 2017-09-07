6bceaed8749df18c63106b6feb351ab663feb9e2
WaitWalker <waitwalker@163.com>
2017-09-07 15:03:24 +0800

    读取本地文件,排序列表

--- MyTwitter/MyTwitter/Modules/Search/MTTSearchViewController.swift
+++ MyTwitter/MyTwitter/Modules/Search/MTTSearchViewController.swift
@@ -21,33 +21,73 @@ class MTTSearchViewController: MTTViewController
 {
     
     var originalTabelView:UITableView?
-    var sectionTitlesArray:NSMutableArray?
+    var sectionTitlesSet:NSMutableSet?
+    var sectionTitlesArray:[String] = []
+    var allCodes:[MTTPhoneAreaCode] = []
+    var codesArray:[MTTPhoneAreaCode] = []
     
-    lazy var codesArray:NSMutableArray = 
+    func setupDataSource() -> Void 
     {
-       let tempArray = NSMutableArray()
-       
-       let codePathURL = Bundle.main.resourceURL?.appendingPathComponent("evt_phone_area_code_zh.txt")
-       let error:NSError
-       
-       //var codeSource:
-       
-       //let codeSource = String.init(contentsOf: codePathURL!, encoding: String.Encoding.utf8)
-       
-//       path(forResource: "evt_phone_area_code_zh", ofType: "txt")
-       
-       
-       return tempArray
-         
-    }()
-    
-    
-    
+        sectionTitlesSet = NSMutableSet()
+        
+        let codePathURL = Bundle.main.resourceURL?.appendingPathComponent("evt_phone_area_code_zh.txt")
+        
+        var codeSource:String = String.init()
+        
+        do {
+            let codeSources = try String.init(contentsOf: codePathURL!, encoding: String.Encoding.utf8)
+            codeSource = codeSources
+        } catch
+        {
+            codeSource = ""
+        }
+        
+        let linesArray = codeSource.components(separatedBy: NSCharacterSet.newlines)
+        
+        for line in linesArray
+        {
+            let parts =  line.components(separatedBy: "=")
+            let code = MTTPhoneAreaCode()
+            if parts.count == 3
+            {
+                code.areaName = parts[0]
+                code.areaAlphaName = parts[1]
+                code.areaCodeName = parts[2]
+            }
+            
+            let stringIndex = code.areaAlphaName?.index((code.areaAlphaName?.startIndex)!, offsetBy: 1)
+            code.areaFirstChar = code.areaAlphaName?.substring(to: stringIndex!)
+            sectionTitlesSet?.add(code.areaFirstChar as Any)
+            allCodes.append(code)
+        }
+        
+        codesArray = allCodes.sorted { (obj1, obj2) -> Bool in
+            obj1.areaFirstChar! < obj2.areaFirstChar!
+        }
+        
+        for code in codesArray
+        {
+            print(code.areaFirstChar as Any)
+            sectionTitlesSet?.add(code.areaFirstChar as Any)
+        }
+        
+        var strings:[String] = []
+        
+        for char in sectionTitlesSet!
+        {
+            strings.append(char as! String)
+        }
+        
+        sectionTitlesArray = strings.sorted { (obj1, obj2) -> Bool in
+            obj1 < obj2
+        }
+        print(sectionTitlesArray)
+    }
     
     override func viewDidLoad() 
     {
         super.viewDidLoad()
-        
+        self.setupDataSource()
     }
     
     func setupSubview() -> Void 

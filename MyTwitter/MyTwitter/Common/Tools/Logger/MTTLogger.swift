53bf27adc6fa142fb682ae01f858afd4047adb79
WaitWalker <waitwalker@163.com>
2017-09-30 17:24:41 +0800

    添加日志拓展文件,下一步进行日志系统功能拓展开发

---
+++ MyTwitter/MyTwitter/Common/Tools/Logger/MTTLogger.swift
@@ -0,0 +1,17 @@
+//
+//  MTTLogger.swift
+//  MyTwitter
+//
+//  Created by LiuChuanan on 2017/9/30.
+//  Copyright © 2017年 waitWalker. All rights reserved.
+//
+
+import Foundation
+import XCGLogger
+
+public struct MTTLogger {}
+
+public extension MTTLogger
+{
+    
+}

18aaf38792e03ac3ae175ef55f58514d6bc5e8d2
WaitWalker <waitwalker@163.com>
2017-10-19 14:48:15 +0800

    多种类型cell布局完成

---
+++ MyTwitter/MyTwitter/Modules/Notification(通知)/View/cell/MTTNotificationMultiTypeCell.swift
@@ -0,0 +1,109 @@
+//
+//  MTTNotificationMultiTypeCell.swift
+//  MyTwitter
+//
+//  Created by LiuChuanan on 2017/10/19.
+//  Copyright © 2017年 waitWalker. All rights reserved.
+//
+
+import UIKit
+
+class MTTNotificationMultiTypeCell: MTTTableViewCell 
+{
+    
+    var lineView:UIView?
+    var iconImageView:UIImageView?
+    var avatarImageViews:UIImageView?
+    var multiTitleLabel:UILabel?
+    var multiContentLabel:UILabel?
+    
+    
+    
+    
+    
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) 
+    {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupSubview()
+        layoutSubview()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupSubview() -> Void 
+    {
+        lineView = UIView()
+        lineView?.backgroundColor = kMainGrayColor()
+        self.contentView.addSubview(lineView!)
+        
+        iconImageView = UIImageView()
+        iconImageView?.isUserInteractionEnabled = true
+        self.contentView.addSubview(iconImageView!)
+        
+        avatarImageViews = UIImageView()
+        avatarImageViews?.isUserInteractionEnabled = true
+        self.contentView.addSubview(avatarImageViews!)
+        
+        multiTitleLabel = UILabel()
+        multiTitleLabel?.textColor = UIColor.black
+        multiTitleLabel?.font = UIFont.systemFont(ofSize: 15)
+        multiTitleLabel?.textAlignment = NSTextAlignment.left
+        self.contentView.addSubview(multiTitleLabel!)
+        
+        multiContentLabel = UILabel()
+        multiContentLabel?.textAlignment = NSTextAlignment.left
+        multiContentLabel?.textColor = kMainGrayColor()
+        multiContentLabel?.font = UIFont.systemFont(ofSize: 15)
+        multiContentLabel?.numberOfLines = 0
+        self.contentView.addSubview(multiContentLabel!)
+    }
+    
+    private func layoutSubview() -> Void 
+    {
+        lineView?.snp.makeConstraints({ (make) in
+            make.top.left.right.equalTo(0)
+            make.height.equalTo(0.3)
+        })
+        
+        iconImageView?.snp.makeConstraints({ (make) in
+            make.left.equalTo(65)
+            make.height.equalTo(20)
+            make.width.equalTo(15)
+            make.top.equalTo(10)
+        })
+        
+        avatarImageViews?.snp.makeConstraints({ (make) in
+            make.left.equalTo((self.iconImageView?.snp.right)!).offset(5)
+            make.height.width.equalTo(40)
+            make.top.equalTo(10)
+        })
+        
+        multiTitleLabel?.snp.makeConstraints({ (make) in
+            make.left.equalTo(self.avatarImageViews!)
+            make.top.equalTo((self.avatarImageViews?.snp.bottom)!).offset(5)
+            make.height.equalTo(20)
+            make.right.equalTo(20)
+        })
+        
+        multiContentLabel?.snp.makeConstraints({ (make) in
+            make.left.equalTo(self.avatarImageViews!)
+            make.right.equalTo(20)
+            make.top.equalTo((self.multiTitleLabel?.snp.bottom)!).offset(0)
+            make.bottom.equalTo(-5)
+        })
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

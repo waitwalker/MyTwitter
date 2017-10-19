eb51dd25f1ad7ff05cf2e187a478f2065de38265
WaitWalker <waitwalker@163.com>
2017-10-19 08:04:10 +0800

    创建通知类型下文件

---
+++ MyTwitter/MyTwitter/Modules/Notification(通知)/Model/MTTNotificationModel.swift
@@ -0,0 +1,14 @@
+//
+//  MTTNotificationModel.swift
+//  MyTwitter
+//
+//  Created by WangJunZi on 2017/10/19.
+//  Copyright © 2017年 waitWalker. All rights reserved.
+//
+
+import UIKit
+
+class MTTNotificationModel: NSObject
+{
+
+}
---
+++ MyTwitter/MyTwitter/Modules/Notification(通知)/ViewModel/MTTNotificationViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MTTNotificationViewModel.swift
+//  MyTwitter
+//
+//  Created by WangJunZi on 2017/10/19.
+//  Copyright © 2017年 waitWalker. All rights reserved.
+//
+
+import UIKit
+
+class MTTNotificationViewModel: NSObject
+{
+
+}

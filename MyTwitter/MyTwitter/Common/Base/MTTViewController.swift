b3aeaf528f36057cc36d8161721c97397246e0a0
WaitWalker <waitwalker@163.com>
2017-10-25 17:58:35 +0800

    增加日间夜间颜色设置

--- MyTwitter/MyTwitter/Common/Base/MTTViewController.swift
+++ MyTwitter/MyTwitter/Common/Base/MTTViewController.swift
@@ -19,6 +19,35 @@ class MTTViewController: UIViewController {
     {
         super.viewDidLoad()
         self.view.backgroundColor = kMainWhiteColor()
+        
+        addNotification()
+    }
+    
+    func addNotification() -> Void 
+    {
+        NotificationCenter.default.addObserver(self, selector: #selector(dayNotificationAction(notify:)), name: NSNotification.Name(rawValue: kDayNotificationString), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(nightNotificationAction(notify:)), name: NSNotification.Name(rawValue: kNightNotificationString), object: nil)
+    }
+    
+    @objc func dayNotificationAction(notify:Notification) -> Void 
+    {
+        self.view.backgroundColor = kMainWhiteColor()
+    }
+    
+    @objc func nightNotificationAction(notify:Notification) -> Void 
+    {
+        self.view.backgroundColor = kMainGreenColor()
+    }
+    
+    private func removeNotification() -> Void 
+    {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    deinit 
+    {
+        removeNotification()
     }
 
     override func didReceiveMemoryWarning() 

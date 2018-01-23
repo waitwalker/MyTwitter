f723f92618b67bf2a3a1225b6b74620ac31cda52
WaitWalker <waitwalker@163.com>
2018-01-23 22:12:39 +0800

    runtime监听系统method swizzle method

--- MyTwitter/MyTwitter/Common/Tools/CrashManager/AvoidCrashManager/Category/NSObject+AvoidCrashCategory.m
+++ MyTwitter/MyTwitter/Common/Tools/CrashManager/AvoidCrashManager/Category/NSObject+AvoidCrashCategory.m
@@ -16,8 +16,13 @@
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         [MTTAvoidCrashManager swizzledInClass:[self class] originalSelector:@selector(setValue:forKey:) swizzledSelector:@selector(avoidCrashSetValue:forKey:)];
+        
+        [MTTAvoidCrashManager swizzledInClass:[self class] originalSelector:@selector(setValue:forKeyPath:) swizzledSelector:@selector(avoidCrashSetValue:forkeyPath:)];
+        
+        [MTTAvoidCrashManager swizzledInClass:[self class] originalSelector:@selector(setValue:forUndefinedKey:) swizzledSelector:@selector(avoidCrashSetValue:forUndefinedKey:)];
     });
 }
+
 
 - (void)avoidCrashSetValue:(id)value forKey:(NSString *)key
 {
@@ -25,6 +30,41 @@
         [self avoidCrashSetValue:value forKey:key];
     }
     @catch(NSException *exception) {
+        NSString *defaultToDo = AvoidCrashDefaultIgnore;
+        [MTTAvoidCrashManager noteErrorWithException:exception defaultToDo:defaultToDo];
+        
+    }
+    @finally {
+        
+    }
+}
+
+- (void)avoidCrashSetValue:(id)value forkeyPath:(NSString *)keyPath
+{
+    @try {
+        [self avoidCrashSetValue:value forkeyPath:keyPath];
+    }
+    @catch(NSException *exception) {
+        NSString *defaultToDo = AvoidCrashDefaultIgnore;
+        [MTTAvoidCrashManager noteErrorWithException:exception defaultToDo:defaultToDo];
+        
+    }
+    @finally {
+        
+    }
+}
+
+- (void)avoidCrashSetValue:(id)value forUndefinedKey:(NSString *)forUndefinedKey
+{
+    @try {
+        [self avoidCrashSetValue:value forUndefinedKey:forUndefinedKey];
+    }
+    @catch(NSException *exception) {
+        NSString *defaultToDo = AvoidCrashDefaultIgnore;
+        [MTTAvoidCrashManager noteErrorWithException:exception defaultToDo:defaultToDo];
+        
+    }
+    @finally {
         
     }
 }
--- MyTwitter/MyTwitter/Common/Tools/CrashManager/AvoidCrashManager/MTTAvoidCrashManager.m
+++ MyTwitter/MyTwitter/Common/Tools/CrashManager/AvoidCrashManager/MTTAvoidCrashManager.m
@@ -8,6 +8,12 @@
 
 #import "MTTAvoidCrashManager.h"
 #import <objc/message.h>
+#define key_errorName        @"errorName"
+#define key_errorReason      @"errorReason"
+#define key_errorPlace       @"errorPlace"
+#define key_defaultToDo      @"defaultToDo"
+#define key_callStackSymbols @"callStackSymbols"
+#define key_exception        @"exception"
 
 @implementation MTTAvoidCrashManager
 
@@ -37,6 +43,91 @@
     {
         method_exchangeImplementations(originalMethod, swizzledMethod);
     }
+}
+
++ (void)noteErrorWithException:(NSException *)exception defaultToDo:(NSString *)defaultToDo
+{
+    //堆栈数据
+    NSArray *callStackSymbolsArr = [NSThread callStackSymbols];
+    
+    //获取在哪个类的哪个方法中实例化的数组  字符串格式 -[类名 方法名]  或者 +[类名 方法名]
+    NSString *mainCallStackSymbolMsg = [MTTAvoidCrashManager getMainCallStackSymbolMessageWithCallStackSymbols:callStackSymbolsArr];
+    
+    if (mainCallStackSymbolMsg == nil) {
+        
+        mainCallStackSymbolMsg = @"崩溃方法定位失败,请您查看函数调用栈来排查错误原因";
+        
+    }
+    
+    NSString *errorName = exception.name;
+    NSString *errorReason = exception.reason;
+    //errorReason 可能为 -[__NSCFConstantString avoidCrashCharacterAtIndex:]: Range or index out of bounds
+    //将avoidCrash去掉
+    errorReason = [errorReason stringByReplacingOccurrencesOfString:@"avoidCrash" withString:@""];
+    
+    NSString *errorPlace = [NSString stringWithFormat:@"Error Place:%@",mainCallStackSymbolMsg];
+    
+    NSString *logErrorMessage = [NSString stringWithFormat:@"\n\n%@\n\n%@\n%@\n%@\n%@",AvoidCrashSeparatorWithFlag, errorName, errorReason, errorPlace, defaultToDo];
+    
+    logErrorMessage = [NSString stringWithFormat:@"%@\n\n%@\n\n",logErrorMessage,AvoidCrashSeparator];
+    AvoidCrashLog(@"%@",logErrorMessage);
+    
+    NSDictionary *errorInfoDic = @{
+                                   key_errorName        : errorName,
+                                   key_errorReason      : errorReason,
+                                   key_errorPlace       : errorPlace,
+                                   key_defaultToDo      : defaultToDo,
+                                   key_exception        : exception,
+                                   key_callStackSymbols : callStackSymbolsArr
+                                   };
+    
+    //将错误信息放在字典里，用通知的形式发送出去
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[NSNotificationCenter defaultCenter] postNotificationName:AvoidCrashNotification object:nil userInfo:errorInfoDic];
+    });
+    
+}
+
++ (NSString *)getMainCallStackSymbolMessageWithCallStackSymbols:(NSArray<NSString *>*)callStackSymbols
+{
+    //mainCallStackSymbolMsg的格式为   +[类名 方法名]  或者 -[类名 方法名]
+    __block NSString *mainCallStackSymbolMsg = nil;
+    
+    //匹配出来的格式为 +[类名 方法名]  或者 -[类名 方法名]
+    NSString *regularExpStr = @"[-\\+]\\[.+\\]";
+    
+    
+    NSRegularExpression *regularExp = [[NSRegularExpression alloc] initWithPattern:regularExpStr options:NSRegularExpressionCaseInsensitive error:nil];
+    
+    
+    for (int index = 2; index < callStackSymbols.count; index++) {
+        NSString *callStackSymbol = callStackSymbols[index];
+        
+        [regularExp enumerateMatchesInString:callStackSymbol options:NSMatchingReportProgress range:NSMakeRange(0, callStackSymbol.length) usingBlock:^(NSTextCheckingResult * _Nullable result, NSMatchingFlags flags, BOOL * _Nonnull stop) {
+            if (result) {
+                NSString* tempCallStackSymbolMsg = [callStackSymbol substringWithRange:result.range];
+                
+                //get className
+                NSString *className = [tempCallStackSymbolMsg componentsSeparatedByString:@" "].firstObject;
+                className = [className componentsSeparatedByString:@"["].lastObject;
+                
+                NSBundle *bundle = [NSBundle bundleForClass:NSClassFromString(className)];
+                
+                //filter category and system class
+                if (![className hasSuffix:@")"] && bundle == [NSBundle mainBundle]) {
+                    mainCallStackSymbolMsg = tempCallStackSymbolMsg;
+                    
+                }
+                *stop = YES;
+            }
+        }];
+        
+        if (mainCallStackSymbolMsg.length) {
+            break;
+        }
+    }
+    
+    return mainCallStackSymbolMsg;
 }
 
 @end

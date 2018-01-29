20b1f04e88362217132284f7a014abd64e25e74b
WaitWalker <waitwalker@163.com>
2018-01-29 21:54:04 +0800

    不可变数组分类添加

---
+++ MyTwitter/MyTwitter/Common/Tools/CrashManager/AvoidCrashManager/Category/NSArray+AvoidCrash.h
@@ -0,0 +1,14 @@
+//
+//  NSArray+AvoidCrash.h
+//  MyTwitter
+//
+//  Created by WangJunZi on 2018/1/29.
+//  Copyright © 2018年 waitWalker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MTTAvoidCrashInterface.h"
+
+@interface NSArray (AvoidCrash)<MTTAvoidCrashInterface>
+
+@end
---
+++ MyTwitter/MyTwitter/Common/Tools/CrashManager/AvoidCrashManager/Category/NSArray+AvoidCrash.m
@@ -0,0 +1,18 @@
+//
+//  NSArray+AvoidCrash.m
+//  MyTwitter
+//
+//  Created by WangJunZi on 2018/1/29.
+//  Copyright © 2018年 waitWalker. All rights reserved.
+//
+
+#import "NSArray+AvoidCrash.h"
+
+@implementation NSArray (AvoidCrash)
+
++ (void)avoidCrashExchangeMethod
+{
+    
+}
+
+@end

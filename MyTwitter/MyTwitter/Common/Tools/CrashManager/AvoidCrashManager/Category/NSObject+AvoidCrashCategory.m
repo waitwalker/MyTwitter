aae587a10b89d1556c25f23fb323f54410ea6c82
WaitWalker <waitwalker@163.com>
2018-01-29 21:47:45 +0800

    NSObject分类文件方法添加完成

--- MyTwitter/MyTwitter/Common/Tools/CrashManager/AvoidCrashManager/Category/NSObject+AvoidCrashCategory.m
+++ MyTwitter/MyTwitter/Common/Tools/CrashManager/AvoidCrashManager/Category/NSObject+AvoidCrashCategory.m
@@ -37,6 +37,52 @@ static NSMutableArray *noneSelClassStringPrefixs;
     });
 }
 
++ (void)setupUnRecognizedClassStringArray:(NSArray<NSString *> *)classStringArray
+{
+    if (noneSelClassStrings)
+    {
+        NSString *warningMsg = [NSString stringWithFormat:@"\n\n%@\n\n[AvoidCrash setupNoneSelClassStringsArr:];\n调用一此即可，多次调用会自动忽略后面的调用\n\n%@\n\n;%@\n;%p\n%@\n",AvoidCrashSeparatorWithFlag,AvoidCrashSeparator,[self class],self,NSStringFromSelector(_cmd)];
+        AvoidCrashLog(@"%@",warningMsg);
+        
+    }
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        noneSelClassStrings = [NSMutableArray array];
+        for (NSString *className in classStringArray) {
+            if ([className hasPrefix:@"UI"] == false && [className isEqualToString:NSStringFromClass([NSObject class])] == false)
+            {
+                [noneSelClassStrings addObject:className];
+            } else
+            {
+                NSString *warningMsg = [NSString stringWithFormat:@"\n\n%@\n\n[AvoidCrash setupNoneSelClassStringsArr:];\n会忽略UI开头的类和NSObject类(请使用NSObject的子类)\n\n%@\n\n;%@\n;%p\n%@\n",AvoidCrashSeparatorWithFlag,AvoidCrashSeparator,[self class],self,NSStringFromSelector(_cmd)];
+                AvoidCrashLog(@"%@",warningMsg);
+            }
+        }
+    });
+}
+
++ (void)setupUnrecognizedClassStringPrefixArray:(NSArray<NSString *> *)classStringPrefixArray
+{
+    if (noneSelClassStringPrefixs) {
+        NSString *warningMsg = [NSString stringWithFormat:@"\n\n%@\n\n[AvoidCrash setupNoneSelClassStringPrefixsArr:];\n调用一此即可，多次调用会自动忽略后面的调用\n\n;%@\n\n%@\n;%p\n%@\n",AvoidCrashSeparatorWithFlag,AvoidCrashSeparator,[self class],self,NSStringFromSelector(_cmd)];
+        AvoidCrashLog(@"%@",warningMsg);
+    }
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        noneSelClassStringPrefixs = [NSMutableArray array];
+        for (NSString *classNamePrefix in classStringPrefixArray) {
+            if ([classNamePrefix hasPrefix:@"UI"] == false && [classNamePrefix hasPrefix:@"NS"] == false)
+            {
+                [noneSelClassStringPrefixs addObject:classNamePrefix];
+            } else
+            {
+                NSString *warningMsg = [NSString stringWithFormat:@"\n\n%@\n\n[AvoidCrash setupNoneSelClassStringsArr:];\n会忽略UI开头的类和NS开头的类\n若需要对NS开头的类防止”unrecognized selector sent to instance”(比如NSArray),请使用setupNoneSelClassStringsArr:\n\n%@\n\n;%@\n;%p\n%@\n",AvoidCrashSeparatorWithFlag,AvoidCrashSeparator,[self class],self,NSStringFromSelector(_cmd)];
+                AvoidCrashLog(@"%@",warningMsg);
+            }
+        }
+    });
+}
 
 - (void)avoidCrashSetValue:(id)value forKey:(NSString *)key
 {

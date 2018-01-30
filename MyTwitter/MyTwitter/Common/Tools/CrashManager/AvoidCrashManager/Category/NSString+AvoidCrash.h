c77de7ce628e677fd29bddbf102186798ee496ed
WaitWalker <waitwalker@163.com>
2018-01-30 21:49:55 +0800

    hook 字符串相关方法

---
+++ MyTwitter/MyTwitter/Common/Tools/CrashManager/AvoidCrashManager/Category/NSString+AvoidCrash.h
@@ -0,0 +1,14 @@
+//
+//  NSString+AvoidCrash.h
+//  MyTwitter
+//
+//  Created by WangJunZi on 2018/1/30.
+//  Copyright © 2018年 waitWalker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MTTAvoidCrashInterface.h"
+
+@interface NSString (AvoidCrash)<MTTAvoidCrashInterface>
+
+@end

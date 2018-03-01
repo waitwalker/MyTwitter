b49bbda53b2c9c7e9b84513ab8bb849a0313f193
WaitWalker <waitwalker@163.com>
2018-03-01 09:27:12 +0800

    添加摇一摇界面

--- MyTwitter/MyTwitter/Modules/Personal(个人中心)/Shake(摇一摇)/Controller/MTTShakeViewController.swift
+++ MyTwitter/MyTwitter/Modules/Personal(个人中心)/Shake(摇一摇)/Controller/MTTShakeViewController.swift
@@ -12,6 +12,14 @@ class MTTShakeViewController: MTTViewController {
     
     var backgroundImageView:UIImageView!
     
+    var topContainerView:UIView!
+    var bottomContainerView:UIView!
+    
+    var topImageView:UIImageView!
+    var bottomImageView:UIImageView!
+    
+    
+    
     
     override func viewWillAppear(_ animated: Bool) 
     {
@@ -35,15 +43,55 @@ class MTTShakeViewController: MTTViewController {
         backgroundImageView = UIImageView()
         backgroundImageView.image = UIImage.imageNamed(name: "shake_back_ground")
         self.view.addSubview(backgroundImageView)
+        
+        topContainerView = UIView()
+        topContainerView.backgroundColor = UIColor.blue
+        self.view.addSubview(topContainerView)
+        
+        bottomContainerView = UIView()
+        bottomContainerView.backgroundColor = UIColor.orange
+        self.view.addSubview(bottomContainerView)
+        
+        topImageView = UIImageView()
+        topImageView.image = UIImage.imageNamed(name: "shake_top")
+        topContainerView.addSubview(topImageView)
+        
+        bottomImageView = UIImageView()
+        bottomImageView.image = UIImage.imageNamed(name: "shake_bottom")
+        bottomContainerView.addSubview(bottomImageView)
     }
     
     func layoutSubview() -> Void 
     {
         backgroundImageView.snp.makeConstraints { make in
             make.center.equalTo(self.view)
-            make.height.equalTo(self.view.width / 2)
+            make.height.equalTo(self.view.height / 2)
             make.width.equalTo(self.view)
         }
+        
+        topContainerView.snp.makeConstraints { make in
+            make.top.left.right.equalTo(self.view)
+            make.height.equalTo(self.view.height / 2)
+        }
+        
+        bottomContainerView.snp.makeConstraints { make in
+            make.left.bottom.right.equalTo(self.view)
+            make.top.equalTo(self.view.height / 2)
+        }
+        
+        topImageView.snp.makeConstraints { make in
+            make.bottom.equalTo(self.topContainerView.snp.bottom)
+            make.centerX.equalTo(self.topContainerView)
+            make.height.equalTo(50)
+            make.width.equalTo(70)
+        }
+        
+        bottomImageView.snp.makeConstraints { make in
+            make.centerX.top.equalTo(self.bottomContainerView)
+            make.height.equalTo(50)
+            make.width.equalTo(70)
+        }
+        
     }
     
     func setupNavigationBar() -> Void 
